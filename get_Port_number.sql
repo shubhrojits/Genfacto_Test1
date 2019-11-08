81270e26d38afbe600203865d8a873bd16bf38bf
shubhrojits <shubhrojits@kovair.com>
2019-11-08 13:57:09 +0530

    First Commit

---
+++ get_Port_number.sql
@@ -0,0 +1,8 @@
+DECLARE @portNumber NVARCHAR(10);
+EXEC xp_instance_regread 
+     @rootkey = 'HKEY_LOCAL_MACHINE', 
+     @key = 'Software\Microsoft\Microsoft SQL Server\MSSQLServer\SuperSocketNetLib\Tcp\IpAll', 
+     @value_name = 'TcpDynamicPorts', 
+     @value = @portNumber OUTPUT;
+SELECT [Port Number] = @portNumber;
+GO
