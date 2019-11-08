cdc513c4da8c2c3ef0ec986e8b886a2197e23ec2
admin <admin@192.168.11.212>
2019-11-08 17:51:57 +0530

    changes from: System 2 Add User Test Before Class construction

--- art01/src/test/java/testCases/Add_User_Page_test.java
+++ art01/src/test/java/testCases/Add_User_Page_test.java
@@ -1,9 +1,14 @@
 package testCases;
 
 import org.testng.annotations.AfterClass;
+import pages.Landing_page;
+import testSuite.SeleniumAbstractTest;
+
 import org.testng.annotations.BeforeClass;
 
-public class Add_User_Page_test {
+public class Add_User_Page_test extends SeleniumAbstractTest {
+	
+	Landing_page lpage = new Landing_page(driver);
 	
 	@BeforeClass
 	public void beforeclass()
