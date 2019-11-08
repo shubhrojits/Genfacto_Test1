45086e581c50b1161c07e1eec5761ab939ae39e1
shubhrojits <shubhrojitsarkar@gmail.com>
2019-11-08 17:37:13 +0530

    Before/After Class added

--- art01/src/test/java/testCases/Add_User_Page_test.java
+++ art01/src/test/java/testCases/Add_User_Page_test.java
@@ -1,5 +1,20 @@
 package testCases;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 public class Add_User_Page_test {
+	
+	@BeforeClass
+	public void beforeclass()
+	{
+		
+	}
+	
+	@AfterClass
+	public void afterclass()
+	{
+		
+	}
 
 }
